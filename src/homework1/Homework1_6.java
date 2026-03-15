37b2c2a9d8fc70c43d34fb410b6b1e679ac56b59
linkus2k <linkus2k@gmail.com>
2026-03-15 21:29:50 +0300

    add Homework 1_6

---
+++ src/homework1/Homework1_6.java
@@ -0,0 +1,17 @@
+package homework1;
+
+public class Homework1_6 {
+
+    public static void main(String[] args) {
+
+        int a = 1;
+        int b = 2;
+
+        a = b/a;
+        b = a/a;
+
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+}
