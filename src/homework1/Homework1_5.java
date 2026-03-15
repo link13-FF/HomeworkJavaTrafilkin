cb144d9711493947853882d71579ce5365bf461e
linkus2k <linkus2k@gmail.com>
2026-03-15 21:13:05 +0300

    add Homework 1_5

---
+++ src/homework1/Homework1_5.java
@@ -0,0 +1,14 @@
+package homework1;
+
+public class Homework1_5 {
+
+    public static void main(String[] args) {
+
+        int q = 111;
+        int w = 92;
+        int a = q/w;
+        int b = q%w;
+
+        System.out.println(q + " / " + w + " = " + a + " и " + b + " в остатке ");
+    }
+}
