80aeaf0445103cc5a087b5333f625d323c4f5eeb
linkus2k <linkus2k@gmail.com>
2026-03-15 19:52:27 +0300

    add Firstname, Lastname and age

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,11 @@
 public class Main {
+
+    public static void main(String[] args) {
+
+        byte age = 35;
+        System.out.println("Трафилькин Валентин Александрович");
+        System.out.println(age);
+
+
+    }
 }
